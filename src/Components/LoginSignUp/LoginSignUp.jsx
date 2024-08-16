520337d218514c3e17b4737e728f7994f9a96e16
Rein-DM <reinier.mercado12@gmail.com>
2024-08-16 21:03:54 +0800

    creation of layout

---
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './LoginSignup.css'
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+export const LoginSignUp = () => {
+  return (
+    <div className='container'>
+        <div className="header">
+            <div className="text">Sign Up</div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            <div className="input">
+                <img src={user_icon} alt="user_icon" />
+                <input type="text" />
+            </div>
+            <div className="input">
+                <img src={email_icon} alt="email_icon" />
+                <input type="email" />
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="password_icon" />
+                <input type="password" />
+            </div>
+        </div>
+        <div className="forgot-password">Lost Password <span>Click Here!</span></div>
+        <div className="submit-container">
+            <div className="submit">Sign Up</div>
+            <div className="submit">Login</div>
+        </div>
+    </div>
+  )
+}
